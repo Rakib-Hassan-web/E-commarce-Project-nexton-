5701fd83b1c32ec80afd61700c93d9ce18250810
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-26 02:37:48 +0600

    login page password input need to validation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -25,7 +25,32 @@ const LoginPage = () => {
     <section id='registationPage' >
         <div className="container ">
             <div className='flex justify-center items-center'>
-              
+               <Form onSubmit={handleSubmit} className='w-[440px] text-center'>
+                {/* -------Header-------- */}
+                <div><h2 className='text-[36px] font-semibold text-second text-center mb-[60px]'>Login</h2></div>
+
+                {/* -------Input Box-------- */}
+                <div className='flex flex-col gap-[24px]'>
+                    {/* -----Email  */}
+                    <div>
+                        <p className='text-base font-semibold text-second'>Email</p>
+                        <p className='text-red-600 font-poppins'>{emailError}</p>
+                        <input  className='w-full border border-BorderCol rounded-[12px] h-[43px] px-5 outline-none mt-2' placeholder='example@example.com' type="email" />
+                    </div>
+                    {/* -----Password  */}
+                  
+                 
+                </div>
+
+                {/* ------Submit Button------ */}
+                <div className='mt-[24px] flex flex-col gap-[24px]'>
+                    <button className='w-full bg-second rounded-full text-white py-[14px] cursor-pointer'>Continue</button>
+                    <p className='text-Primary font-medium text-[14px] text-center'>OR</p>
+                    <div>
+                        <p className='text-Primary text-base font-normal text-center'>New user? <Link className='text-[#0EA5E9]' to={'/RegisterPage'}>Create an account </Link></p>
+                    </div>
+                </div>
+            </Form>
              </div>
              <Link></Link>
 
