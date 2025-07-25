945792be30db9847a1aa33d9e5b3abe750b91a74
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-26 01:55:18 +0600

    cart a image name price add done

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -19,13 +19,15 @@ axios.get('https://api.escuelajs.co/api/v1/products')
 
 .catch((error)=>{console.log(error)})
 } ,[])
- 
+
 const CartAll =product.filter((item)=>{
-  return localCart.includes(item.id)
+return localCart.includes(item.id)
 })
 
 
-console.log(CartAll)
+const totalPrice =CartAll.reduce((sum , no)=>{
+    return sum + no.price
+},0)
 
 return (
 <>
@@ -34,9 +36,9 @@ return (
 
     <section id='addTOCart'>
 
-<div  onClick={closeDiv} className='w-full h-screen bg-[#00000046] absolute top-0 left-0 z-12'></div>
+        <div onClick={closeDiv} className='w-full h-screen bg-[#00000046] absolute top-0 left-0 z-12'></div>
 
-          <div className='bg-white z-20 pt-18 w-[500px] h-screen absolute top-0 right-0'> 
+        <div className='bg-white z-90 pt-18 w-[500px] h-screen absolute top-0 right-0'>
             {/* -------------cart Heading------------ */}
             <div className="Cart_heading flex  items-center  justify-around gap-55">
                 <h2 className='text-[30px] font-semibold font-praymary text-second pl-5'>AddToCart</h2>
@@ -45,40 +47,42 @@ return (
             </div>
             <div className="allProuct   h-[610px]  overflow-y-scroll">
 
+                {/* -----------single product map---------- */}
+
                 {
-                    CartAll.map((item )=>(
+                CartAll.map((item )=>(
 
                 <div key={item.id} className='flex  items-center justify-around  mt-5'>
-                    <div className="singleProduct flex gap-4 items-center">
-                        <div className="productimg w-[70px] h-[70px]  rounded-3xl bg-praymary ">
-                           <img src={item.images[0]} alt="Productimage" className='rounded-3xl' />
+                    <div className="singleProduct flex gap-4 items-center pl-10">
+                        <div className="productimg w-[70px] h-[70px]  rounded-3xl bg-praymary mr-4 ">
+                            <img src={item.images[0]} alt="Productimage" className='rounded-3xl ' />
                         </div>
 
-                        <h2 className='text-base text-second font-medium font-praymary overflow-ellipsis'>{item.title}</h2>
+                        <h2 id='catrproname' className=' w-[200px] text-base text-second font-medium font-praymary overflow-ellipsis '>{item.title}
+                        </h2>
                     </div>
 
                     <div className="product_price text-md text-second font-medium font-praymary ">{item.price}$</div>
 
                 </div>
-                    ))
+                ))
                 }
-                {/* -----------single product---------- */}
-               
+
             </div>
 
-                {/* -----------total_price------- */}
-                <div className="totalPrice flex items-center gap-59 mb-4">
-                    <h2 className='product_price text-[23px] text-second font-medium font-praymary pl-10'>Total Price :
-                    </h2>
-                    <h2 className='product_price text-[20px] text-second font-medium font-praymary'>160$</h2>
+            {/* -----------total_price------- */}
+            <div className="totalPrice flex items-center gap-59 mb-4">
+                <h2 className='product_price text-[23px] text-second font-medium font-praymary pl-10'>Total Price :
+                </h2>
+                <h2 className='product_price text-[20px] text-second font-medium font-praymary'>{totalPrice}$</h2>
 
-                </div>
-                {/* ------------check out button---------- */}
-                <Link
-                    className='w-full text-center bg-black mt-[5px] inline-block py-4 text-2xl text-white font-praymary font-semibold'>
-                Check Out</Link>
+            </div>
+            {/* ------------check out button---------- */}
+            <Link
+                className='w-full text-center bg-black mt-[5px] inline-block py-4 text-2xl text-white font-praymary font-semibold'>
+            Check Out</Link>
 
-                        </div>
+        </div>
 
 
 
@@ -100,27 +104,3 @@ return (
 }
 
 export default AddToCart
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
