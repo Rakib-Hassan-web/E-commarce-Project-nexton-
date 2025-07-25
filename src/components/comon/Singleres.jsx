ea9feaa75af713e187ec69567b603beda1bf4427
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-25 23:32:44 +0600

    cart icon added

--- src/components/comon/Singleres.jsx
+++ src/components/comon/Singleres.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import { HiStar } from 'react-icons/hi'
+import { IoCartOutline } from 'react-icons/io5'
+import { PiShoppingCartSimpleThin } from 'react-icons/pi'
 
 const Singleres = ({ pname , pprice , pimage ,prating , pacce , pdiscount ,Showdetails}) => {
 return (
 <>
 
+{/* onClick={cartClick} */}
 
 
+  {/* <section id='recomandation'>
 
-  <section id='recomandation'>
+    <div className="container"  */}
+         <div className=' relative  h-[448px] w-[320px]'>
+             
+               <button  className='w-[40px] h-[40px] bg-orange-400 rounded-full flex justify-center items-center text-2xl text-white font-bold absolute right-[10px] top-[10px] cursor-pointer hover:bg-primary duration-300 z-20'><PiShoppingCartSimpleThin /></button>
 
-    <div className="container" onClick={Showdetails}>
+       
+      <div className='w-[320px] h-[448px]  rounded-[16px]   ' onClick={Showdetails}>
 
-
-      <div className='w-[320px] h-[448px]  rounded-[16px]'>
-
-        <div className="recoimg w-full h-[360px]  rounded-[16px] overflow-hidden mt-4">
-          <img src={pimage} alt="product image" />
+        <div className="recoimg w-full h-[360px]  rounded-[16px] overflow-hidden mt-4  z-1 ">
+          <img src={pimage} alt="product image"   />
         </div>
-
+          
         <div className="recoAllTxt">
-
+         
 
           <div className="reco1sttxtrow flex items-center justify-between   ">
             <h2 className='font-praymary font-semibold text-second text-base'>{pname}</h2>
@@ -39,17 +44,18 @@ return (
         </div>
 
 
+</div>
+
 
 
 
       </div>
-
-
+{/* 
     </div>
 
 
 
-  </section>
+  </section> */}
 
 
 
