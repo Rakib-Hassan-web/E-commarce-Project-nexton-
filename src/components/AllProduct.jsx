8670afa6c4539d093594a16bb46a88759638fd05
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-10-17 23:00:55 +0600

    responsive

--- src/components/AllProduct.jsx
+++ src/components/AllProduct.jsx
@@ -1,77 +1,89 @@
 import React, { useEffect, useState } from 'react'
 import Singleres from './comon/Singleres'
 import axios from 'axios'
-import Pagination from './Pagination';
-import { useNavigate } from 'react-router';
-import { useSelector } from 'react-redux';
+import Pagination from './Pagination'
+import { useNavigate } from 'react-router'
+import { useSelector } from 'react-redux'
 
-const AllProduct = ({Showdetails}) => {
-    const reduxProduct =useSelector((state)=> state.product.value)
-console.log(reduxProduct)
+const AllProduct = ({ Showdetails }) => {
+  const reduxProduct = useSelector((state) => state.product.value)
+  console.log(reduxProduct)
 
+  const [products, setProducts] = useState([])
+  const [page, setPage] = useState(1)
+  const itemsPerPage = 6
 
-const [products, setProducts] = useState([]);
-const [page, setPage] = useState(1);
-const itemsPerPage = 6;
+  useEffect(() => {
+    axios
+      .get("https://api.escuelajs.co/api/v1/products")
+      .then((res) => {
+        if (!reduxProduct) return setProducts(res.data)
+        const FilterP = res.data.filter((item) => item.title === reduxProduct)
+        setProducts(FilterP)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [reduxProduct])
 
-useEffect(() => {
-axios
-.get("https://api.escuelajs.co/api/v1/products")
-.then((res) => {
-  if(!reduxProduct) return setProducts(res.data)
+  const navigate = useNavigate()
 
+  const handelShow = (Info) => {
+    navigate(`/Product/${Info.id}`)
+    console.log(Info)
+  }
 
-    const FilterP =res.data.filter((item)=>item.title==reduxProduct)
-    setProducts(FilterP)
-})
-.catch((err) => {
-console.log(err);
-});
-}, [reduxProduct]);
+  // --------------local storage--------
+  const handlecart = (data) => {
+    const myArray = JSON.parse(localStorage.getItem('ProductKey')) || []
+    myArray.push(data)
+    localStorage.setItem('ProductKey', JSON.stringify(myArray))
+  }
 
+  const start = (page - 1) * itemsPerPage
+  const currentItems = products.slice(start, start + itemsPerPage)
+  const totalPages = Math.ceil(products.length / itemsPerPage)
 
-const navigate = useNavigate()
-
-let handelShow =(Info)=>{
-  navigate(`/Product/${Info.id}`)
-  console.log(Info)
-}
-
-
-
-// --------------local storage--------
-
-const handlecart =(data)=>{
-
-  const myArray = JSON.parse(localStorage.getItem('ProductKey')) ||  []
-
-  myArray.push(data)
-  localStorage.setItem( 'ProductKey' , JSON.stringify(myArray))
-}
-
-
-
-const start = (page - 1) * itemsPerPage;
-const currentItems = products.slice(start, start + itemsPerPage);
-const totalPages = Math.ceil(products.length / itemsPerPage);
-return (
-<>
-    <div className='flex items-end flex-wrap justify-end'>
-        <div className=' lg:flex mt-4 lg:flex-wrap justify-between items-center '>
-            {
-            currentItems.map((item,i)=>(
-            <Singleres key={i} cartclick={()=>handlecart(item.id)}  Showdetails={()=>handelShow(item)} className={'overflow-ellipsis'} pimage={item.images[0]} pname={item.category.slug}
-                pprice={item.price} pacce={item.title } />
-            ))
-            }
+  return (
+    <>
+      <div className='w-full'>
+        {/* ----------Product grid---------- */}
+        <div
+          className='
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-2 
+            lg:grid-cols-3 
+            gap-6 
+            mt-6
+          '
+        >
+          {currentItems.map((item, i) => (
+            <Singleres
+              key={i}
+              cartclick={() => handlecart(item.id)}
+              Showdetails={() => handelShow(item)}
+              className={'overflow-ellipsis'}
+              pimage={item.images[0]}
+              pname={item.category.slug}
+              pprice={item.price}
+              pacce={item.title}
+            />
+          ))}
         </div>
-            <Pagination totalPages={totalPages} currentPage={page} setPage={setPage} />
 
-
-    </div>
-
-</>
-)
+        {/* ----------Pagination---------- */}
+        <div className='flex justify-center mt-10'>
+          <Pagination
+            totalPages={totalPages}
+            currentPage={page}
+            setPage={setPage}
+          />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AllProduct
